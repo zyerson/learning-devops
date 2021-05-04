1f8cd16d53a58d3ae64411e734f332d54d58a4ca
yerson <yerson.zuniga@servinformacion.com>
2021-05-03 19:06:45 -0500

    modificacion 2

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -11,7 +11,7 @@ public class BasicApplicationTests {
 
 	@Test
 	public void contextLoads() {
-		String meessage= "default message cambio 11";
+		String meessage= "default message cambio test";
 		Assert.assertNotNull(meessage);
 	}
 
